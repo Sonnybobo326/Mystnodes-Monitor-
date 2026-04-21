574be24b4957b796e2fa34cbe2981dcfc11c5885
Bryan Johnson <137770853+Sonnybobo326@users.noreply.github.com>
2026-04-21 13:43:59 -0500

    Set up Express server with basic route

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 10000;
+
+app.get('/', (req, res) => {
+  res.send('Mystnodes Monitor is running!');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
